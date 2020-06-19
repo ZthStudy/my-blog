78002f73a632beb7afe2f6db74adb45e5da365e3
ZthStudy <30645249+ZthStudy@users.noreply.github.com>
2020-06-19 15:44:17 +0800

    调整图床为路过图床

--- web/src/pages/MyLove/index.vue
+++ web/src/pages/MyLove/index.vue
@@ -4,7 +4,7 @@
  * @Author: zhangtianhou
  * @Date: 2020-06-15 16:03:20
  * @LastEditors: zhangtianhou
- * @LastEditTime: 2020-06-19 14:24:50
+ * @LastEditTime: 2020-06-19 15:43:38
 --> 
 <template>
   <div class="mylove-container">
@@ -55,26 +55,29 @@ export default defineComponent({
           {
             subTitle: "亚龙湾",
             timestamp: "2020/5/16",
+
             imageSrcs: [
-              "https://ae01.alicdn.com/kf/H2be5d3044fad446a93e7b1a9b681bd821.jpg",
-              "https://ae01.alicdn.com/kf/He10b9abc64b54ca2bd38e18f00941c42s.jpg",
-              "https://ae01.alicdn.com/kf/H2e384c29e12948728d5095791e0bd367r.jpg",
-              "https://ae01.alicdn.com/kf/Hf6526e128cd547ffa1df5d256f0bf76dO.jpg",
-              "https://ae01.alicdn.com/kf/Hb575e41188474cd39dc709fa61376b35D.jpg"
+              "https://s1.ax1x.com/2020/06/19/NKQ3Jf.jpg",
+              "https://s1.ax1x.com/2020/06/19/NKQtyQ.jpg",
+              "https://s1.ax1x.com/2020/06/19/NKQYQg.jpg",
+              "https://s1.ax1x.com/2020/06/19/NKQ8W8.jpg",
+              "https://s1.ax1x.com/2020/06/19/NKQJSS.jpg"
             ]
           },
           {
             subTitle: "天堂森林公园",
             timestamp: "2020/5/17",
             imageSrcs: [
-              "https://ae01.alicdn.com/kf/H7da3974145d7460f8d662d9357cd3f05e.jpg",
-              "https://ae01.alicdn.com/kf/Hc75435a5579140c58fd827c2e8b51e14T.jpg",
-              "https://ae01.alicdn.com/kf/H9c6eb9d9f8cc460da6ad63e88da034eeX.jpg",
-              "https://ae01.alicdn.com/kf/Hf0c18ce6f1b944708189549d39e459edD.jpg",
-              "https://ae01.alicdn.com/kf/H2fa57aca8c3b4c949a3c73a6692ceb2av.jpg",
-              "https://ae01.alicdn.com/kf/H6a996f05bcce43209ff33fb4bfa76705D.jpg",
-              "https://ae01.alicdn.com/kf/H026df1abc3274aa88def00c2fd96474cq.jpg",
-              "https://ae01.alicdn.com/kf/Hf2cd1728a0b1478899e0d9f0755b949aP.jpg",
+              "https://s1.ax1x.com/2020/06/19/NKnZHx.jpg",
+              "https://s1.ax1x.com/2020/06/19/NKnPCF.jpg",
+              "https://s1.ax1x.com/2020/06/19/NKnFgJ.jpg",
+              "https://s1.ax1x.com/2020/06/19/NKn94U.jpg",
+              "https://s1.ax1x.com/2020/06/19/NKnEuR.jpg",
+              "https://s1.ax1x.com/2020/06/19/NKnVD1.jpg",
+              "https://s1.ax1x.com/2020/06/19/NKnu4O.jpg",
+              "https://s1.ax1x.com/2020/06/19/NKnmE6.jpg",
+              "https://s1.ax1x.com/2020/06/19/NKu7lQ.jpg",
+              "https://s1.ax1x.com/2020/06/19/NKuHyj.jpg"
             ]
           }
         ]
